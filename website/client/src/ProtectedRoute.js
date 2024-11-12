d9c10a75643153dad8372584a961f435dae917ac
momen165 <35774864+momen165@users.noreply.github.com>
2024-11-12 22:46:40 +0200

    Refactor routing and authentication logic to include admin access control and improve route organization

--- website/client/src/ProtectedRoute.js
+++ website/client/src/ProtectedRoute.js
@@ -2,14 +2,22 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from './AuthContext'; // Adjust path if necessary
 
-const ProtectedRoute = ({ children }) => {
+const ProtectedRoute = ({ children, adminRequired = false }) => {
   const { user } = useAuth();
 
+  // Check if the user is logged in
   if (!user) {
     return <Navigate to="/LoginPage" replace />;
   }
-  console.log(user) 
+
+  // If the route requires admin access, check if the user is an admin
+  if (adminRequired && !user.admin) {
+    return <Navigate to="/Unauthorized" replace />;
+    console.log('Unauthorized access');
+  }
+
   return children;
 };
+
 
 export default ProtectedRoute;
