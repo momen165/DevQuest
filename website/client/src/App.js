5af2b4a29b834c367db3c8bef3c65bfd42d60397
momen165 <35774864+momen165@users.noreply.github.com>
2024-11-13 22:33:13 +0200

    Add JWT decoding and admin access control to authentication flow

--- website/client/src/App.js
+++ website/client/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from 'pages/user/HomePage';
 import CoursesPage from 'pages/user/CoursesPage';
-import CourseSection from 'pages/user/CourseSection';
 import EnrollmentPage from 'pages/user/EnrollmentPage';
 import FAQPage from 'pages/user/FAQPage';
 import PricingPage from 'pages/user/PricingPage';
@@ -16,16 +15,19 @@ import Billing from 'pages/user/Billing';
 import Students from 'pages/admin/Students';
 import AdminCourses from 'pages/admin/AdminCourses';
 import ProtectedRoute from './ProtectedRoute'; // Import ProtectedRoute
+import { useAuth } from 'AuthContext';
 import './App.css';
 
 function App() {
+  const { user } = useAuth();
+  console.log('User in App:', user); // Log user to verify its value
+
   return (
     <Router>
       <Routes>
         {/* Public Routes */}
         <Route path="/" element={<HomePage />} />
         <Route path="/CoursesPage" element={<CoursesPage />} />
-        
         <Route path="/enroll/:courseId" element={<EnrollmentPage />} />
         <Route path="/faq" element={<FAQPage />} />
         <Route path="/pricing" element={<PricingPage />} />
@@ -34,15 +36,14 @@ function App() {
         <Route path="/ForgotPasswordPage" element={<ForgotPasswordPage />} />
 
         {/* Admin Routes (Protected for Admins) */}
-        <Route path="/Dashboard" element={<ProtectedRoute adminRequired><Dashboard /></ProtectedRoute>} />
-        <Route path="/Students" element={<ProtectedRoute adminRequired><Students /></ProtectedRoute>} />
-        <Route path="/AdminCourses" element={<ProtectedRoute adminRequired><AdminCourses /></ProtectedRoute>} />
+        <Route path="/Dashboard" element={<ProtectedRoute adminRequired={true}><Dashboard /></ProtectedRoute>} />
+        <Route path="/Students" element={<ProtectedRoute adminRequired={true}><Students /></ProtectedRoute>} />
+        <Route path="/AdminCourses" element={<ProtectedRoute adminRequired={true}><AdminCourses /></ProtectedRoute>} />
 
         {/* Protected Routes for Users */}
         <Route path="/Profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
         <Route path="/ChangePassword" element={<ProtectedRoute><ChangePassword /></ProtectedRoute>} />
         <Route path="/Billing" element={<ProtectedRoute><Billing /></ProtectedRoute>} />
-        <Route path="/CourseSection/:courseId" element={<ProtectedRoute><CourseSection /></ProtectedRoute>} />
       </Routes>
     </Router>
   );
