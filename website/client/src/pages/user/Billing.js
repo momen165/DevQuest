079b58addd251790c4e9d356b49beb8521b397c4
momen165 <35774864+momen165@users.noreply.github.com>
2024-11-14 20:12:04 +0200

    Remove unused imports and clean up Billing and ProfilePage components

--- website/client/src/pages/user/Billing.js
+++ website/client/src/pages/user/Billing.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'styles/Billing.css'; // Create and import this CSS file for styling
 import Navbar from 'components/Navbar';
-import { Link } from 'react-router-dom';
+
 import Sidebar from 'components/AccountSettingsSidebar';
 
 function Billing() {
