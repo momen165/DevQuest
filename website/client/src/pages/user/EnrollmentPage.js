704be24a289b33ff3c4d0aa46acb6a2fbd9a0611
Yazan-Isam-Qa <161217218+Yazan-Isam-Qa@users.noreply.github.com>
2024-12-09 19:42:00 +0000

    edited the styles of the lesson page , added some functions to the course carde which is the enroll btn will change if the user is the user is already enrolled to the course

--- website/client/src/pages/user/EnrollmentPage.js
+++ website/client/src/pages/user/EnrollmentPage.js
@@ -24,7 +24,6 @@ const EnrollmentPage = () => {
         setCourse(data);
 
         // Check if the user is already enrolled in the course
-        ///courses/:course_id/enrollments/:user_id
         const enrollmentResponse = await fetch(`/api/courses/${courseId}/enrollments/${user.user_id}`);
         if (!enrollmentResponse.ok) {
           throw new Error('Failed to check enrollment status');
