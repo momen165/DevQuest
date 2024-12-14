a501d1eb0df2107a459b243bcb4dbd2fb16956b5
Yazan-Isam-Qa <161217218+Yazan-Isam-Qa@users.noreply.github.com>
2024-12-14 19:10:07 +0000

    payment configertion edits

--- website/server/routes/subscription.routes.js
+++ website/server/routes/subscription.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { addSubscription, getSubscriptions, cancelSubscription, updateSubscription } = require('../controllers/subscription.controller');
+const { addSubscription, getSubscriptions, cancelSubscription, updateSubscription, listSubscriptions, retrieveSubscription, retrieveSubscriptionFromStripe } = require('../controllers/subscription.controller');
 const authenticateToken = require('../middleware/auth'); // Add your authentication middleware
 
 const router = express.Router();
@@ -8,5 +8,8 @@ router.post('/subscribe', authenticateToken, addSubscription);
 router.get('/subscriptions', authenticateToken, getSubscriptions);
 router.delete('/subscription', authenticateToken, cancelSubscription); // Add the new route
 router.put('/subscription', authenticateToken, updateSubscription); // Add the new route
+router.get('/list-subscriptions', authenticateToken, listSubscriptions); // Add the new route
+router.get('/subscription/:id', authenticateToken, retrieveSubscription); // Add the new route
+router.get('/stripe-subscription/:subscriptionId', authenticateToken, retrieveSubscriptionFromStripe); // Add the new route
 
 module.exports = router;
