de983bf118925d7893165dff54b32cac8bed778e
Yazan-Isam-Qa <161217218+Yazan-Isam-Qa@users.noreply.github.com>
2024-12-26 11:34:53 +0000

    feat: Add animated logo components and update styles across pages
    - Added new animated logo assets (`animatedLogo.svg`, `animatedLogoSecond.svg`, `footerLogo.svg`).
    - Created `AnimatedLogo` component and corresponding styles.
    - Introduced `FeedbackCardScroll` component for enhanced feedback display.
    - Updated `FAQSection`, `Footer`, and `slider` components with new styling and functionality.
    - Modified `CoursesPage`, `HomePage`, and associated CSS for layout improvements.
    - Enhanced feedback functionality in `feedback.controller.js` and `feedback.routes.js`.
    - Updated `package.json` and `package-lock.json` to reflect new dependencies.

--- website/server/routes/feedback.routes.js
+++ website/server/routes/feedback.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getFeedback, submitFeedback, getCoursesWithRatings, replyToFeedback } = require('../controllers/feedback.controller');
+const { getFeedback, submitFeedback, getCoursesWithRatings, replyToFeedback, getPublicFeedback } = require('../controllers/feedback.controller');
 const authenticateToken = require('../middleware/auth'); // Add your authentication middleware
 
 const router = express.Router();
@@ -9,4 +9,6 @@ router.post('/feedback', authenticateToken, submitFeedback);
 router.post('/feedback/reply', authenticateToken, replyToFeedback);
 
 router.get('/getCoursesWithRatings',  getCoursesWithRatings);
+router.get('/feedback/public', getPublicFeedback);
+
 module.exports = router;
