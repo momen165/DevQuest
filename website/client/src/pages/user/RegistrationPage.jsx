9ab057d11c8ae96c79f539d2e6f8f5edb255c77d
momen165 <35774864+momen165@users.noreply.github.com>
2025-01-05 14:01:42 +0200

    refactor: Update authentication page styles and remove deprecated CSS
    - Replaced 'AuthPage.css' with 'AuthPages.css' across ForgotPasswordPage, LoginPage, and RegistrationPage components for consistent styling.
    - renamed  'AuthPage.css' to  'AuthPages.css'

--- website/client/src/pages/user/RegistrationPage.jsx
+++ website/client/src/pages/user/RegistrationPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import 'styles/AuthPage.css';
+import 'styles/AuthPages.css';
 import {useNavigate} from 'react-router-dom';
 import { useAuth } from 'AuthContext';
 
